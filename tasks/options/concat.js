c35a43c2a3b9651e883144589c6b73c49ba585ed
Tim Wright <timwright12@gmail.com>
2016-04-14 14:40:55 -0400

    Update concat.js

--- tasks/options/concat.js
+++ tasks/options/concat.js
@@ -1,8 +1,4 @@
 module.exports = {
-  main: {
-    src: ['src/assets/js/app.js'],
-    dest: 'src/assets/js/app.min.js',
-  },
   vendor: {
     src: ['src/assets/js/vendor/*.js'],
     dest: 'src/assets/js/vendor.concat.js',
